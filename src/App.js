2a9e3cf7c645322b934151f0916b7de8a48d0c3b
Omen Uchiha <omen@LikeMindss-MacBook-Pro.local>
2023-04-13 17:08:28 +0530

    A

--- src/App.js
+++ src/App.js
@@ -90,14 +90,14 @@ function App() {
   const [community, setCommunity] = useState({});
   useEffect(() => {
     // 2nd arg for userid
-    initiateSDK(false, "", "")
+    initiateSDK(false, "36011b36-dc6d-43b3-ae68-9251f36063ac", "")
       .then((res) => {
         setCommunity(res?.data?.community);
         setCurrentUser(res?.data?.user);
         sessionStorage.setItem("communityId", res?.data?.community?.id);
       })
       .catch((error) => {
-        // // console.log("Error =>", error);
+        console.log("Error =>", error);
       });
   }, []);
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 import LikeMinds from "likeminds-chat-beta";
 import { log } from "./sdkFunctions";
 
 export const myClient = new LikeMinds({
-  // apiKey: "d4356d31-306e-406d-aa4a-cd49f1b88f19",
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: "d4356d31-306e-406d-aa4a-cd49f1b88f19",
+  // apiKey: process.env.REACT_APP_API_KEY,
   xPlatformCode: process.env.REACT_APP_XPLATFORM_CODE,
-  xVersionCode: 9999,
+  xVersionCode: 22,
   baseUrl: process.env.REACT_APP_BASE_URL,
   baseUrlCaravan: process.env.REACT_APP_BASE_URL_CARAVAN,
 });
@@ -18,8 +18,6 @@ log(myClient);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
-reportWebVitals();
 
 export const UserContext = React.createContext({
   currentUser: null,
