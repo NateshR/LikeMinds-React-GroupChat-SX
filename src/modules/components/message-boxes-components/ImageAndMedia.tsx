0f9372b6cabed586f62167e1f6d79a4c65117dd6
omenUchiha <gaurav.rajput@likeminds.community>
2024-02-02 15:18:50 +0530

    fixed remaining conflicts

--- src/modules/components/message-boxes-components/ImageAndMedia.tsx
+++ src/modules/components/message-boxes-components/ImageAndMedia.tsx
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
 import { PropsWithChildren } from "react";
-import './imageAndMedia.css'
-=======
->>>>>>> release/v1.12.0
+import "./imageAndMedia.css";
 type ImageAndMediaType = {
   mediaArray: any;
   setMediaDisplayModel: any;
   setMediaData: any;
 };
-<<<<<<< HEAD
 const ImageAndMedia: React.FC<ImageAndMediaType> = ({
   mediaArray,
   setMediaDisplayModel,
   setMediaData,
 }) => (
-=======
-const ImageAndMedia = ({
-  mediaArray,
-  setMediaDisplayModel,
-  setMediaData,
-}: ImageAndMediaType) => (
->>>>>>> release/v1.12.0
   <div className="flex w-full">
     {mediaArray?.length === 1 ? (
       <div
@@ -51,94 +40,68 @@ const ImageAndMedia = ({
     ) : (
       <>
         <div
-<<<<<<< HEAD
           // className="max-w-[50%] h-full grow"
           // className=""
-=======
           className="max-w-[50%] h-full grow"
->>>>>>> release/v1.12.0
           onClick={() => {
             setMediaData({ mediaObj: mediaArray, type: "image" });
             setMediaDisplayModel(true);
           }}
         >
-<<<<<<< HEAD
-         
-            <div className="multipleImagesContainer">
-              <div className="primaryImage imgBubble">
-                {mediaArray[0].type === "image" ?<img
+          <div className="multipleImagesContainer">
+            <div className="primaryImage imgBubble">
+              {mediaArray[0].type === "image" ? (
+                <img
                   src={mediaArray[0].url!}
                   alt=""
                   className="max-w-full max-h-full block h-auto w-auto"
-                />:   <video
-                // controls={false}
-              
-                className="max-w-full max-h-full block h-auto w-auto"
-                key={mediaArray[0]?.url}
-              //   onClick={}
-              >
-                <source src={mediaArray[0]?.url} type="video/mp4" />
-                <source src={mediaArray[0]?.url} type="video/ogg" />
-                Your browser does not support the video tag.
-              </video>
-             }
-              </div>
-              <div className="secondaryImage imgBubble">
-                <p className="foregroundCover">+{(mediaArray.length - 1).toString()}</p>
-                {mediaArray[1].type === "image" ?<img
+                />
+              ) : (
+                <video
+                  // controls={false}
+
+                  className="max-w-full max-h-full block h-auto w-auto"
+                  key={mediaArray[0]?.url}
+                  //   onClick={}
+                >
+                  <source src={mediaArray[0]?.url} type="video/mp4" />
+                  <source src={mediaArray[0]?.url} type="video/ogg" />
+                  Your browser does not support the video tag.
+                </video>
+              )}
+            </div>
+            <div className="secondaryImage imgBubble">
+              <p className="foregroundCover">
+                +{(mediaArray.length - 1).toString()}
+              </p>
+              {mediaArray[1].type === "image" ? (
+                <img
                   src={mediaArray[1].url!}
                   alt=""
                   className="max-w-full max-h-full block h-auto w-auto"
-                />:    <video
-                
-                
-                className="max-w-full max-h-full block h-auto w-auto"
-                key={mediaArray[1]?.url}
-              //   onClick={}
-              >
-                <source src={mediaArray[1]?.url} type="video/mp4" />
-                <source src={mediaArray[1]?.url} type="video/ogg" />
-                Your browser does not support the video tag.
-              </video>}
-              </div>
+                />
+              ) : (
+                <video
+                  className="max-w-full max-h-full block h-auto w-auto"
+                  key={mediaArray[1]?.url}
+                  //   onClick={}
+                >
+                  <source src={mediaArray[1]?.url} type="video/mp4" />
+                  <source src={mediaArray[1]?.url} type="video/ogg" />
+                  Your browser does not support the video tag.
+                </video>
+              )}
             </div>
-           
+          </div>
         </div>
         {/* <div
-=======
-          {mediaArray[0].type === "image" ? (
-            <img
-              src={mediaArray[0].url!}
-              alt=""
-              className="max-w-full max-h-full block h-auto w-auto"
-            />
-          ) : (
-            <video
-              controls
-              preload="none"
-              className="max-w-full max-h-full block h-auto w-auto"
-              key={mediaArray[0]?.url}
-              //   onClick={}
-            >
-              <source src={mediaArray[0]?.url} type="video/mp4" />
-              <source src={mediaArray[0]?.url} type="video/ogg" />
-              Your browser does not support the video tag.
-            </video>
-          )}
-        </div>
-        <div
->>>>>>> release/v1.12.0
           className="max-w-[50%] grow flex justify-center items-center bg-slate-400"
           style={{ opacity: "50%" }}
         >
           <span className="text-xl text-black  text-center">
             + {(mediaArray.length - 1).toString()}
           </span>
-<<<<<<< HEAD
         </div> */}
-=======
-        </div>
->>>>>>> release/v1.12.0
       </>
     )}
   </div>
